d11eb4074449e0f72c8b2bbdde85f8f5c7f710d0
ehco1996 <zh19960202@gmail.com>
2018-08-24 15:55:25 +0800

    fix for pep8

---
+++ constants.py
@@ -0,0 +1,28 @@
+SIZE = 500
+GRID_LEN = 4
+GRID_PADDING = 10
+
+BACKGROUND_COLOR_GAME = "#92877d"
+BACKGROUND_COLOR_CELL_EMPTY = "#9e948a"
+
+BACKGROUND_COLOR_DICT = {2: "#eee4da", 4: "#ede0c8", 8: "#f2b179",
+                         16: "#f59563", 32: "#f67c5f", 64: "#f65e3b",
+                         128: "#edcf72", 256: "#edcc61", 512: "#edc850",
+                         1024: "#edc53f", 2048: "#edc22e"}
+
+CELL_COLOR_DICT = {2: "#776e65", 4: "#776e65", 8: "#f9f6f2", 16: "#f9f6f2",
+                   32: "#f9f6f2", 64: "#f9f6f2", 128: "#f9f6f2",
+                   256: "#f9f6f2", 512: "#f9f6f2", 1024: "#f9f6f2",
+                   2048: "#f9f6f2"}
+
+FONT = ("Verdana", 40, "bold")
+
+KEY_UP_ALT = "\'\\uf700\'"
+KEY_DOWN_ALT = "\'\\uf701\'"
+KEY_LEFT_ALT = "\'\\uf702\'"
+KEY_RIGHT_ALT = "\'\\uf703\'"
+
+KEY_UP = "'w'"
+KEY_DOWN = "'s'"
+KEY_LEFT = "'a'"
+KEY_RIGHT = "'d'"
