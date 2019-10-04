0963d4541e2246c308271037a83a1b75b478abe6
Zalán <toth.zalan03@gmail.com>
2019-10-04 18:51:48 +0200

    Update constants.py

--- constants.py
+++ constants.py
@@ -29,8 +29,8 @@ KEY_LEFT_ALT = "\'\\uf702\'"
 KEY_RIGHT_ALT = "\'\\uf703\'"
 
 KEY_UP = "'w'"
-KEY_DOWN  = "'s'"
-KEY_LEFT  = "'a'"
+KEY_DOWN = "'s'"
+KEY_LEFT = "'a'"
 KEY_RIGHT = "'d'"
 KEY_BACK = "'b'"
 
