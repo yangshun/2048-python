b48ebd54b93880c1b59f17dc0f61fd15082a1ec3
rajat-rg <72245885+rajat-rg@users.noreply.github.com>
2020-10-02 13:18:44 +0530

    Update constants.py
    added a few comments in file for readability

--- constants.py
+++ constants.py
@@ -1,3 +1,4 @@
+# This program file is about declaring constants and variable
 SIZE = 400
 GRID_LEN = 4
 GRID_PADDING = 10
@@ -5,7 +6,7 @@ GRID_PADDING = 10
 BACKGROUND_COLOR_GAME = "#92877d"
 BACKGROUND_COLOR_CELL_EMPTY = "#9e948a"
 
-BACKGROUND_COLOR_DICT = {2: "#eee4da", 4: "#ede0c8", 8: "#f2b179",
+BACKGROUND_COLOR_DICT = { 2: "#eee4da", 4: "#ede0c8", 8: "#f2b179",
                          16: "#f59563", 32: "#f67c5f", 64: "#f65e3b",
                          128: "#edcf72", 256: "#edcc61", 512: "#edc850",
                          1024: "#edc53f", 2048: "#edc22e",
@@ -13,7 +14,7 @@ BACKGROUND_COLOR_DICT = {2: "#eee4da", 4: "#ede0c8", 8: "#f2b179",
                          4096: "#eee4da", 8192: "#edc22e", 16384: "#f2b179",
                          32768: "#f59563", 65536: "#f67c5f", }
 
-CELL_COLOR_DICT = {2: "#776e65", 4: "#776e65", 8: "#f9f6f2", 16: "#f9f6f2",
+CELL_COLOR_DICT = { 2: "#776e65", 4: "#776e65", 8: "#f9f6f2", 16: "#f9f6f2",
                    32: "#f9f6f2", 64: "#f9f6f2", 128: "#f9f6f2",
                    256: "#f9f6f2", 512: "#f9f6f2", 1024: "#f9f6f2",
                    2048: "#f9f6f2",
@@ -23,10 +24,14 @@ CELL_COLOR_DICT = {2: "#776e65", 4: "#776e65", 8: "#f9f6f2", 16: "#f9f6f2",
 
 FONT = ("Verdana", 40, "bold")
 
+#movement keys
+
 KEY_UP_ALT = "\'\\uf700\'"
 KEY_DOWN_ALT = "\'\\uf701\'"
 KEY_LEFT_ALT = "\'\\uf702\'"
 KEY_RIGHT_ALT = "\'\\uf703\'"
+
+# control keys
 
 KEY_UP = "'w'"
 KEY_DOWN = "'s'"
