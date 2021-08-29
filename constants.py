803077103c512ccd2309d942b62d9cf342e17869
hexadeci-male <69492910+hexadeci-male@users.noreply.github.com>
2021-08-29 05:02:14 -0600

    Changed controls & did house keeping (#20)

--- constants.py
+++ constants.py
@@ -5,36 +5,60 @@ GRID_PADDING = 10
 BACKGROUND_COLOR_GAME = "#92877d"
 BACKGROUND_COLOR_CELL_EMPTY = "#9e948a"
 
-BACKGROUND_COLOR_DICT = {2: "#eee4da", 4: "#ede0c8", 8: "#f2b179",
-                         16: "#f59563", 32: "#f67c5f", 64: "#f65e3b",
-                         128: "#edcf72", 256: "#edcc61", 512: "#edc850",
-                         1024: "#edc53f", 2048: "#edc22e",
+BACKGROUND_COLOR_DICT = {
+2:      "#eee4da",
+4:      "#ede0c8",
+8:      "#f2b179",
+16:     "#f59563",
+32:     "#f67c5f",
+64:     "#f65e3b",
+128:    "#edcf72",
+256:    "#edcc61",
+512:    "#edc850",
+1024:   "#edc53f",
+2048:   "#edc22e",
+4096:   "#eee4da",
+8192:   "#edc22e",
+16384:  "#f2b179",
+32768:  "#f59563",
+65536:  "#f67c5f",
+}
 
-                         4096: "#eee4da", 8192: "#edc22e", 16384: "#f2b179",
-                         32768: "#f59563", 65536: "#f67c5f", }
+CELL_COLOR_DICT = {
+2:      "#776e65",
+4:      "#776e65",
+8:      "#f9f6f2",
+16:     "#f9f6f2",
+32:     "#f9f6f2",
+64:     "#f9f6f2",
+128:    "#f9f6f2",
+256:    "#f9f6f2",
+512:    "#f9f6f2",
+1024:   "#f9f6f2",
+2048:   "#f9f6f2",
+4096:   "#776e65",
+8192:   "#f9f6f2",
+16384:  "#776e65",
+32768:  "#776e65",
+65536:  "#f9f6f2",
+}
 
-CELL_COLOR_DICT = {2: "#776e65", 4: "#776e65", 8: "#f9f6f2", 16: "#f9f6f2",
-                   32: "#f9f6f2", 64: "#f9f6f2", 128: "#f9f6f2",
-                   256: "#f9f6f2", 512: "#f9f6f2", 1024: "#f9f6f2",
-                   2048: "#f9f6f2",
+FONT = ("Verdana",40,"bold")
 
-                   4096: "#776e65", 8192: "#f9f6f2", 16384: "#776e65",
-                   32768: "#776e65", 65536: "#f9f6f2", }
+KEY_QUIT = "Escape"
+KEY_BACK = "b"
 
-FONT = ("Verdana", 40, "bold")
+KEY_UP = "Up"
+KEY_DOWN = "Down"
+KEY_LEFT = "Left"
+KEY_RIGHT = "Right"
 
-KEY_UP_ALT = "\'\\uf700\'"
-KEY_DOWN_ALT = "\'\\uf701\'"
-KEY_LEFT_ALT = "\'\\uf702\'"
-KEY_RIGHT_ALT = "\'\\uf703\'"
+KEY_UP_ALT1 = "w"
+KEY_DOWN_ALT1 = "s"
+KEY_LEFT_ALT1 = "a"
+KEY_RIGHT_ALT1 = "d"
 
-KEY_UP = "'w'"
-KEY_DOWN = "'s'"
-KEY_LEFT = "'a'"
-KEY_RIGHT = "'d'"
-KEY_BACK = "'b'"
-
-KEY_J = "'j'"
-KEY_K = "'k'"
-KEY_L = "'l'"
-KEY_H = "'h'"
+KEY_UP_ALT2 = "i"
+KEY_DOWN_ALT2 = "k"
+KEY_LEFT_ALT2 = "j"
+KEY_RIGHT_ALT2 = "l"
--- puzzle.py
+++ puzzle.py
@@ -1,13 +1,10 @@
 from tkinter import Frame, Label, CENTER
 import random
-
 import logic
 import constants as c
 
-
 def gen():
     return random.randint(0, c.GRID_LEN - 1)
-
 
 class GameGrid(Frame):
     def __init__(self):
@@ -17,13 +14,21 @@ class GameGrid(Frame):
         self.master.title('2048')
         self.master.bind("<Key>", self.key_down)
 
-        self.commands = {c.KEY_UP: logic.up, c.KEY_DOWN: logic.down,
-                         c.KEY_LEFT: logic.left, c.KEY_RIGHT: logic.right,
-                         c.KEY_UP_ALT: logic.up, c.KEY_DOWN_ALT: logic.down,
-                         c.KEY_LEFT_ALT: logic.left, c.KEY_RIGHT_ALT: logic.right,
-                         c.KEY_H: logic.left, c.KEY_L: logic.right,
-                         c.KEY_K: logic.up, c.KEY_J: logic.down}
-        
+        self.commands = {
+            c.KEY_UP: logic.up,
+            c.KEY_DOWN: logic.down,
+            c.KEY_LEFT: logic.left,
+            c.KEY_RIGHT: logic.right,
+            c.KEY_UP_ALT1: logic.up,
+            c.KEY_DOWN_ALT1: logic.down,
+            c.KEY_LEFT_ALT1: logic.left,
+            c.KEY_RIGHT_ALT1: logic.right,
+            c.KEY_UP_ALT2: logic.up,
+            c.KEY_DOWN_ALT2: logic.down,
+            c.KEY_LEFT_ALT2: logic.left,
+            c.KEY_RIGHT_ALT2: logic.right,
+        }
+
         self.grid_cells = []
         self.init_grid()
         self.matrix = logic.new_game(c.GRID_LEN)
@@ -33,24 +38,34 @@ class GameGrid(Frame):
         self.mainloop()
 
     def init_grid(self):
-        background = Frame(self, bg=c.BACKGROUND_COLOR_GAME,
-                           width=c.SIZE, height=c.SIZE)
+        background = Frame(self, bg=c.BACKGROUND_COLOR_GAME,width=c.SIZE, height=c.SIZE)
         background.grid()
 
         for i in range(c.GRID_LEN):
             grid_row = []
             for j in range(c.GRID_LEN):
-                cell = Frame(background, bg=c.BACKGROUND_COLOR_CELL_EMPTY,
-                             width=c.SIZE / c.GRID_LEN,
-                             height=c.SIZE / c.GRID_LEN)
-                cell.grid(row=i, column=j, padx=c.GRID_PADDING,
-                          pady=c.GRID_PADDING)
-                t = Label(master=cell, text="",
-                          bg=c.BACKGROUND_COLOR_CELL_EMPTY,
-                          justify=CENTER, font=c.FONT, width=5, height=2)
+                cell = Frame(
+                    background,
+                    bg=c.BACKGROUND_COLOR_CELL_EMPTY,
+                    width=c.SIZE / c.GRID_LEN,
+                    height=c.SIZE / c.GRID_LEN
+                )
+                cell.grid(
+                    row=i,
+                    column=j,
+                    padx=c.GRID_PADDING,
+                    pady=c.GRID_PADDING
+                )
+                t = Label(
+                    master=cell,
+                    text="",
+                    bg=c.BACKGROUND_COLOR_CELL_EMPTY,
+                    justify=CENTER,
+                    font=c.FONT,
+                    width=5,
+                    height=2)
                 t.grid()
                 grid_row.append(t)
-
             self.grid_cells.append(grid_row)
 
     def update_grid_cells(self):
@@ -58,20 +73,25 @@ class GameGrid(Frame):
             for j in range(c.GRID_LEN):
                 new_number = self.matrix[i][j]
                 if new_number == 0:
-                    self.grid_cells[i][j].configure(text="", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
+                    self.grid_cells[i][j].configure(text="",bg=c.BACKGROUND_COLOR_CELL_EMPTY)
                 else:
-                    self.grid_cells[i][j].configure(text=str(new_number), bg=c.BACKGROUND_COLOR_DICT[new_number],
-                                                    fg=c.CELL_COLOR_DICT[new_number])
+                    self.grid_cells[i][j].configure(
+                        text=str(new_number),
+                        bg=c.BACKGROUND_COLOR_DICT[new_number],
+                        fg=c.CELL_COLOR_DICT[new_number]
+                    )
         self.update_idletasks()
 
     def key_down(self, event):
-        key = repr(event.char)
+        key = event.keysym
+        print(event)
+        if key == c.KEY_QUIT: exit()
         if key == c.KEY_BACK and len(self.history_matrixs) > 1:
             self.matrix = self.history_matrixs.pop()
             self.update_grid_cells()
             print('back on step total step:', len(self.history_matrixs))
         elif key in self.commands:
-            self.matrix, done = self.commands[repr(event.char)](self.matrix)
+            self.matrix, done = self.commands[key](self.matrix)
             if done:
                 self.matrix = logic.add_two(self.matrix)
                 # record last move
@@ -89,6 +109,5 @@ class GameGrid(Frame):
         while self.matrix[index[0]][index[1]] != 0:
             index = (gen(), gen())
         self.matrix[index[0]][index[1]] = 2
-
 
 game_grid = GameGrid()
